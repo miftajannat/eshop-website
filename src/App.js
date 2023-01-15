38e1f0c53f8a3d5f014f5528c5031d45861507f7
miftajannat <miftajannat.dev@gmail.com>
2023-01-15 08:56:32 +0600

    home page is completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
               
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
